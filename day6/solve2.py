fdf8b36a5117b4f8648e50e16bc7cb5615a79912
icosac <enricosaccon96@gmail.com>
2021-12-10 17:51:29 +0100

    Still working on second part of day 7

--- day6/solve2.py
+++ day6/solve2.py
@@ -1,28 +1,51 @@
+import matplotlib.pyplot as plt
+from math import log2 as log
+import numpy as np
+
 def main():
     fishes=list()
-    days=10
+    days=np.arange(1, 144, 6)
     
-    with open("input2.txt", "r") as f:
+    with open("input.txt", "r") as f:
         for line in f:
             for fish in line.split("\n")[0].split(","):
                 fishes.append(int(fish))
     
-    for i in range(1, days):
-        for j in range(len(fishes)):
-            if fishes[j]>0:
-                fishes[j]-=1
-            else:
-                fishes[j]=6
-                fishes.append(8)
-        print(i, len(fishes))
-
-    tot_fishes=len(fishes)
+    print(fishes)
+    setFishes=list()
     for fish in fishes:
-        remaining_days=days-fish
-        created_fishes=int(remaining_days/6)
-        for i in range(created_fishes):
-            remaining_days-=8
-            created_fishes+=(remaining_days/6)
+        if fish not in setFishes:
+            setFishes.append(fish)
+    print(setFishes)
+
+    setFishes=[0]
+    newFishes=dict()
+    fishperday=list()
+    for day in days:
+        for fish in setFishes:
+            newFishes[str(fish)]=0
+            compFishes=[fish]
+            for i in range(1, day+1):
+                for j in range(len(compFishes)):
+                    if compFishes[j]>0:
+                        compFishes[j]-=1
+                    else:
+                        compFishes[j]=6
+                        compFishes.append(8)
+                #print((setFishes.index(fish)+i)*100/(len(setFishes)+256))
+            
+            newFishes[str(fish)]=len(compFishes)
+            fishperday.append(len(compFishes))
+        print(day, newFishes)
+
+    plt.scatter(days, [value for value in fishperday])
+    plt.show()
+
+    tot=0
+    for fish in fishes:
+        tot+=newFishes[str(fish)]
+
+    print(tot)
 
 
 
