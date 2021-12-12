2d9951fcbdcee1bcbaf5dc41442603b6a770efdb
icosac <enricosaccon96@gmail.com>
2021-12-12 17:56:55 +0100

    Day 11 and 12. The latter's second part is driving me crazy

---
+++ day11/solve1.py
@@ -0,0 +1,92 @@
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+class Map:
+    def __init__(self):
+        self.map = [[-2 ** 32], [-2 ** 32]]
+
+    def add_line(self, line):
+        self.map.insert(-1, [-2 ** 32])
+        for v in line:
+            self.map[-2].append(int(v))
+        self.map[-2].append(-2 ** 32)
+        self.resize(len(line))
+
+    def resize(self, n):
+        self.map[0] = [-2 ** 32 for i in range(n + 2)]
+        self.map[-1] = [-2 ** 32 for i in range(n + 2)]
+        for i in range(len(self.map)):
+            if len(self.map[i]) != (n + 2):
+                raise Exception("Wrong dimension", i, len(self.map[i]), n+2)
+
+    def flash(self, octo_list, x, y):
+        for octoX in range(x - 1, x + 2):
+            for octoY in range(y - 1, y + 2):
+                if (octoX, octoY) not in octo_list:
+                    try:
+                        self.map[octoX][octoY] += 1
+                    except Exception as E:
+                        print(E, octoX, octoY, self)
+                    if self.map[octoX][octoY] > 9:
+                        octo_list.append((octoX, octoY))
+                        self.flash(octo_list, octoX, octoY)
+
+    def map_flash(self):
+        to_flash = list()
+        # First, the energy level of each octopus increases by 1
+        for octoX in range(1, len(self.map) - 1):
+            for octoY in range(1, len(self.map[octoX]) - 1):
+                self.map[octoX][octoY] += 1
+
+        # Then, any octopus with an energy level greater than 9 flashes
+        for octoX in range(1, len(self.map) - 1):
+            for octoY in range(1, len(self.map[octoX]) - 1):
+                if self.map[octoX][octoY] > 9 and (octoX, octoY) not in to_flash:
+                    to_flash.append((octoX, octoY))
+                    self.flash(to_flash, octoX, octoY)
+
+        for (x, y) in to_flash:
+            self.map[x][y] = 0
+
+        return to_flash
+
+    def __str__(self):
+        ret = "\n"
+        for octoLine in self.map:
+            for octo in octoLine:
+                if octo < -1 :
+                    pass
+                    #ret += "B"
+                elif octo==0:
+                    ret += color.RED+str(octo)+color.END
+                else:
+                    ret += str(octo)
+            ret += "\n"
+
+        return ret
+
+def main():
+    steps=100
+    octoMap = Map()
+    with open("input.txt", "r") as f:
+        for line in f:
+            octoMap.add_line(line=line.split("\n")[0])
+    flashes=0
+    for step in range(steps):
+        flashes+=len(octoMap.map_flash())
+    print(octoMap)
+    print(flashes)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day12/solve1.py
@@ -0,0 +1,112 @@
+from copy import deepcopy
+
+class Node:
+    def __init__(self, name:str):
+        self.name=name
+        # start=0, end=-1, small=1, big=-2
+        if self.name=="start":
+            self.dim=0
+        elif self.name=="end":
+            self.dim=-1
+        elif self.name.islower():
+            self.dim=1
+        else:
+            self.dim=-2
+        self.neighbors=list()
+        self.children=list()
+        self.parents=list()
+
+    def __eq__(self, other):
+        if self.name==other.name:
+            return True
+        else:
+            return False
+
+    def addNeighbor(self, node):
+        if node not in self.neighbors:
+            self.neighbors.append(node)
+            self.children.append(node)
+            self.parents.append(node)
+        else:
+            raise Exception(node, "already present in neighbors:", self.neighbors)
+
+    def __str__(self):
+        ret=self.name+": "
+        for node in self.neighbors:
+            ret+=node.name+", "
+        return ret
+
+def printNodes(lst, string=""):
+    print(string, end='')
+    for node in lst:
+        print(node.name, end=", ")
+    print()
+
+def valid(path):
+    for i in range(len(path)):
+        if (path[i].islower() and path[i] in path[i+1:]
+                or path[i]==Node("start")):
+            return False
+    return True
+
+class Graph:
+    def __init__(self, nodes):
+        self.nodes=nodes
+
+    def __str__(self):
+        ret=""
+        for node in self.nodes:
+            ret+=str(node)
+            ret+="\n"
+        return ret
+
+    def find_paths(self, node):
+        paths=[]
+        if node==Node("end"):
+            return [[self.nodes[self.nodes.index(Node("end"))]]]
+        else:
+            for nd in node.neighbors:
+                if nd.dim!=0:
+                    nd.dim-=1
+                    below_paths=self.find_paths(nd)
+                    try:
+                        for path in below_paths:
+                            paths.append([node]+path)
+                    except Exception as E:
+                        print(E, path, type(path), below_paths)
+                    nd.dim+=1
+
+            # print("paths:", len(paths))
+            # for path in paths:
+            #     printNodes(path)
+            # print("end!!!")
+            return paths
+
+
+def main():
+    graph=list()
+    with open("input.txt", "r") as f:
+        for line in f:
+            A=Node(line.split("\n")[0].split("-")[0])
+            B=Node(line.split("\n")[0].split("-")[1])
+
+            # Check if node is in graph
+            if B not in graph:
+                graph.append(B)
+            if A not in graph:
+                graph.append(A)
+            # Now I have a reference to the node in the graph
+            A = graph[graph.index(A)]
+            B = graph[graph.index(B)]
+            A.addNeighbor(B)
+            B.addNeighbor(A)
+    graph=Graph(graph)
+
+    print(graph)
+    print(len(graph.find_paths(graph.nodes[graph.nodes.index(Node("start"))])))
+
+
+
+
+if __name__ == '__main__':
+    main()
